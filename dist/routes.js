16dfae1099cfe46bcf08d5f4de772b3290316f00
Equipo4 Cecyte <prog4bt4@gmail.com>
2025-05-21 19:15:08 +0000

    Cambio en la router

--- dist/routes.js
+++ dist/routes.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Routes = void 0;
+const sabor_routes_1 = require("./modules/sabor/routes/sabor.routes");
 const usuario_routes_1 = require("./modules/usuario/routes/usuario.routes");
 class Routes {
     constructor() {
         this.usuarioRoutes = new usuario_routes_1.UsuarioRoutes();
+        this.saborRoutes = new sabor_routes_1.SaborRoutes();
     }
     routes(app) {
         this.usuarioRoutes.routes(app);
+        this.saborRoutes.routes(app);
     }
 }
 exports.Routes = Routes;
--- lib/routes.ts
+++ lib/routes.ts
@@ -1,10 +1,13 @@
+import { SaborRoutes } from "./modules/sabor/routes/sabor.routes";
 import { UsuarioRoutes } from "./modules/usuario/routes/usuario.routes";
 
 export class Routes {
 
     private usuarioRoutes: UsuarioRoutes = new UsuarioRoutes();
+    private saborRoutes: SaborRoutes = new SaborRoutes(); 
 
     public routes(app): void {
         this.usuarioRoutes.routes(app);
+        this.saborRoutes.routes(app);
     }
 }
